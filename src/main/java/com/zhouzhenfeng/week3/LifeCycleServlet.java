b6508cbccce2d29a3a312d1b499186db401c8436
idAnxinBrook <2020211001001213@ecjtu.edu.cn>
2022-06-07 09:06:09 +0800

    week3

---
+++ src/main/java/com/zhouzhenfeng/week3/LifeCycleServlet.java
@@ -0,0 +1,67 @@
+package com.zhouzhenfeng.week3;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+@WebServlet("/life")
+
+public class LifeCycleServlet extends HttpServlet {
+
+
+    Connection con =null;
+    public LifeCycleServlet(){
+        System.out.println("i am in constructor --> LifeCycleServlet() ");
+    }
+    @Override
+    public void init() throws ServletException{
+        ServletContext context=getServletContext();
+        String driver=context.getInitParameter("driver");
+        String url=context.getInitParameter("url");
+        String username=context.getInitParameter("username");
+        String password=context.getInitParameter("password");
+
+
+        try {
+            Class.forName(driver);
+            con= DriverManager.getConnection(url,username,password);
+            System.out.println("Connection --> in JDBCDemoServlet"+con);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("i am in init()->LifeCycleServlet--> "+con);
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("i am in service () --> doGet() ");
+
+    }
+
+    @Override
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+
+    @Override
+    public void destroy() {
+        System.out.println("i am in destory ()");
+        try {
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/zhouzhenfeng/week3/RegisterServlet.java
+++ src/main/java/com/zhouzhenfeng/week3/RegisterServlet.java
@@ -1,110 +1,125 @@
 package com.zhouzhenfeng.week3;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
-
-@WebServlet(urlPatterns = {"/register"},loadOnStartup = 1)
+//@WebServlet(urlPatterns={"/register"},loadOnStartup = 1)
 public class RegisterServlet extends HttpServlet {
-    Connection con= null;
+    Connection con = null;
     @Override
-    public void  init() throws ServletException{
+    public void init() throws ServletException{
         super.init();
-        /*ServletContext context=getServletContext();
+        /*ServletContext context =getServletContext();
         String driver=context.getInitParameter("driver");
         String url=context.getInitParameter("url");
         String username=context.getInitParameter("username");
         String password=context.getInitParameter("password");
         try {
             Class.forName(driver);
-            con = DriverManager.getConnection(url,username,password);
-            System.out.println("init --> "+con);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
+            con= DriverManager.getConnection(url,username,password);
+            System.out.println("Connection --> in JDBCDemoServlet"+con);
+        } catch (Exception e) {
             e.printStackTrace();
         }*/
-       con= (Connection) getServletContext().getAttribute("con");
+        con= (Connection) getServletContext().getAttribute("con");
     }
 
     @Override
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-request.getRequestDispatcher("WEB-INF/views/register.jsp").forward(request,response);
-    }
+        request.getRequestDispatcher("WEB-INF/views/register.jsp").forward(request,response);
 
+
+        //System.out.println("register serlvet doGet");
+        //response.sendRedirect("./register.jsp");
+    }
     @Override
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+
+
+        //System.out.println("register servlet dopost");
         PrintWriter writer = response.getWriter();
+
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         String email = request.getParameter("email");
         String gender = request.getParameter("gender");
         String birthDate = request.getParameter("birthDate");
-        try {
-            Statement st = con.createStatement();
-            String sql = "insert into usertable(username,password,email,gender,birthDate)" +
-                    "values(' "+username+" ',' "+password+" ',' "+email+" ',' "+gender+" ','" + birthDate+"')";
+//        java.sql.Date releaseDate = java.sql.Date.valueOf(birthDate);
+        System.out.println(username);
+        System.out.println(password);
+        System.out.println(email);
+        System.out.println(gender);
+        System.out.println(birthDate);
 
-            System.out.println("sql" + sql);
-            int n = st.executeUpdate(sql);
-            System.out.println("n-->" + n);
-            //sql = "insert username,password,email,gender,birthDate from usertable";
+        try {
+            Statement st=con.createStatement();
+            String sql = "insert into usertable (username,password,email,gender,birthDate) " +
+                    "values(\'"+username+"\',\'" +password+"\',\'"+email+"\',\'"+gender+"\',\'"+birthDate+"\')";
+            System.out.println("sql"+sql);
+
+            int n= st.executeUpdate(sql);
+            System.out.println("n-->"+n);
+
+            //con.setAutoCommit(false);
+            //PreparedStatement preparedStatement = con.prepareStatement(sql);
+            //preparedStatement.executeUpdate();
+            //con.commit();
+            //System.out.println("insert successfully");
+            //sql = "select * from usertable";
+
+            //con.setAutoCommit(false);
+
+            //PreparedStatement preparedStatement = con.prepareStatement(sql);
             //ResultSet rs = st.executeQuery(sql);
             //PrintWriter out = response.getWriter();
-            /*out.println("<html><title></title><body><table border=1><tr>");
-            out.println("<td>username</td><td>password</td><td>email</td><td>gender</td><td>birthDate</td>");
-            while (rs.next()) {
-                out.println("<tr>");
-                out.println("<td>" + rs.getString("username") + "</td>");
-                out.println("<td>" + rs.getString("password") + "</td>");
-                out.println("<td>" + rs.getString("email") + "</td>");
-                out.println("<td>" + rs.getString("gender") + "</td>");
-                out.println("<td>" + rs.getString("birthDate") + "</td>");
-                out.println("</tr>");
-            }
-            out.println("</table></body></html>");*/
+
+            //writer.println("<table border=\"1\">");
+            //writer.println("<tr>");
+            //writer.println("<th>ID</th>");
+            //writer.println("<th>username</th>");
+            //writer.println("<th>password</th>");
+            //writer.println("<th>email</th>");
+            //writer.println("<th>gender</th>");
+            //writer.println("<th>birthDate</th>");
+            //writer.println("</tr>");
+            /*while (resultSet.next()) {
+                writer.println("<tr>");
+                writer.println("<td>"+resultSet.getString("id")+"</td>");
+                writer.println("<td>"+resultSet.getString("username")+"</td>");
+                writer.println("<td>"+resultSet.getString("password")+"</td>");
+                writer.println("<td>"+ resultSet.getString("email")+"</td>");
+                writer.println("<td>"+resultSet.getString("gender")+"</td>");
+                writer.println("<td>"+ resultSet.getString("birthDate")+"</td>");
+                writer.println("<tr>");
+            }*/
+            //writer.println("</table>");
+            //con.commit();
+
+            //writer.close();
             //request.setAttribute("rsname",rs);
+
             //request.getRequestDispatcher("userList.jsp").forward(request,response);
-            //System.out.println("I am in RegisterServlet-->doPost()-->after forward");
+            //System.out.println("i am in RegisterServlet-->doPost()--> after forward()");
+
+            //response.sendRedirect("login.jsp");
+
 
             response.sendRedirect("login");
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
 
-        /*writer.println("<br>username :"+username);
-        writer.println("<br>password :"+password);
-        writer.println("<br>email :"+email);
-        writer.println("<br>gender :"+gender);
-        writer.println("<br>birthDate :"+birthDate);
-        writer.close();*/
 
-        /*try{
-            Class.forName(driverName);
-            con=DriverManager.getConnection(url,userName,userPwd);
-            sql=con.createStatement();
-            sql.executeUpdate("insert into users values("+"'"+username+"'"+","+"'"+password+"'"+")");
-            con.close();
-        }
-        catch(Exception e){
-            System.out.print(e+"\n");
-        }
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("insert success!");
-        out.flush();
-        out.close();
-    }
 
-    public void destroy() {
-        try {
-            con.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-    }*/
     }
 }
